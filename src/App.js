1a1532d1766b3b58af7f481ec3ac99ac141d519a
brithe00 <bthellier51@gmail.com>
2020-10-05 14:33:58 +0200

    build(questionsjs): build questionjs and refactored appjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import questions from '../src/data/questions';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Data dans la console: </h1>
+        {console.log(questions)}
+      </div>
     </div>
   );
 }
