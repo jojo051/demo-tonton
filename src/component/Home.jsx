8473733ea2043fc43b2e7cd686c0224970a22f5a
brithe00 <bthellier51@gmail.com>
2020-10-05 16:16:32 +0200

    build(questionqcm): build question qcm component

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import QuestionPhrase from './QuestionPhrase';
+import QuestionQCM from './QuestionQCM';
 
 function Home() {
   return (
     <>
       <h1>Hello from Home</h1>
       <QuestionPhrase />
+      <QuestionQCM />
     </>
   );
 }
