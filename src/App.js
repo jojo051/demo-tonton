511d6924dc95539b8f31feb8b56d9472c063ecbc
jojo051 <youpi051@yahoo.fr>
2020-10-08 13:58:51 +0200

    fix app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import QuestionPhrase from './component/QuestionPhrase';
+import QuestionSelect from './component/QuestionSelect';
 import Home from './component/Home';
 import QCM from './component/QCM';
-<<<<<<< HEAD
-import questionSelect from './component/QuestionSelect';
-=======
->>>>>>> feat/brianTest
 
 function App() {
   return (
@@ -15,16 +12,9 @@ function App() {
       <Router>
         <Switch>
           {/*<Route path="/5" component={} />
-<<<<<<< HEAD
           <Route path="/4" component={} />*/}
           <Route path="/3" component={QuestionPhrase} />
-          <Route path="/2" component={questionSelect} />
-=======
-          <Route path="/4" component={} />
-          <Route path="/3" component={} />
-          <Route path="/2" component={} />*/}
-          <Route path="/questionphrase" component={QuestionPhrase} />
->>>>>>> feat/brianTest
+          <Route path="/2" component={QuestionSelect} />
           <Route path="/1" component={QCM} />
           <Route path="/" component={Home} />
         </Switch>
